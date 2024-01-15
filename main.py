555b47126f92a587a399351d713eb1208f3bc4b1
Dariazhuravleva <darya.spring@mail.ru>
2024-01-15 14:35:20 +0400

    debugging CRUD

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,21 +18,27 @@ def all_posts():
 
 @app.post("/new_posts")
 def new_posts():
-    responce = requests.get(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/new_posts")
+    responce = requests.post(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/new_posts",
+                             json=request.json,
+                             headers={"Content-Type": "application/json"})
     return responce.json()
 
 
 @app.put("/posts/<int:id>")
 def update_posts(id):
-    responce = requests.get(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/posts/<int:id")
+    responce = requests.put(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/posts/{id}",
+                            json=request.json,
+                            headers={"Content-Type": "application/json"})
     return responce.json()
     
 
 @app.delete("/posts/<int:id>")
 def delete_posts(id):
-     responce = requests.get(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/delete_posts/<int:id")
+     responce = requests.delete(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/delete_posts/{id}",
+                             json=request.json,
+                             headers={"Content-Type": "application/json"})
      return responce.json()
  
      
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
