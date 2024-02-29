8ecb56abc7793eb7332d254ed7cdad66cc964fbb
Daria Zhuravleva <darya.spring@mail.ru>
2024-02-29 14:58:38 +0400

    create crud for comments and checks

--- controller/controller.py
+++ controller/controller.py
@@ -1,8 +1,9 @@
 import os
-import requests
 import uuid
 
-from flask import Flask, request
+import requests
+from flask import request
+
 from controller import app
 from controller.utils import check_login
 
@@ -30,16 +31,16 @@ def update_posts(id):
                             json=request.json,
                             headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
     return responce.json()
-    
+
 
 @app.delete("/posts/<int:id>")
 @check_login
 def delete_posts(id):
-     responce = requests.delete(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/posts/{id}",
-                             json=request.json,
-                             headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
-     return responce.json()
- 
+    responce = requests.delete(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/posts/{id}",
+                               json=request.json,
+                               headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
+    return responce.json()
+
 
 @app.post("/sign_up")
 def sign_up():
@@ -61,8 +62,8 @@ def sign_in():
 @check_login
 def change_password():
     responce = requests.put(f"http://{os.getenv('USERS_SERVICE_HOST')}:{os.getenv('USERS_SERVICE_PORT')}/change_password",
-                             json=request.json,
-                             headers={"Content-Type": "application/json"})
+                            json=request.json,
+                            headers={"Content-Type": "application/json"})
     return responce.json()
 
 
@@ -70,4 +71,36 @@ def change_password():
 @check_login
 def delete_user(id):
     responce = requests.delete(f"http://{os.getenv('USERS_SERVICE_HOST')}:{os.getenv('USERS_SERVICE_PORT')}/delete_user/{id}")
+    return responce.json()
+
+
+@app.get("/comments/<int:id>")
+def get_comments(id):
+    responce = requests.get(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/comments/{id}")
+    return responce.json()
+
+
+@app.post("/comments/<int:id>")
+def new_comment(id):
+    responce = requests.post(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/comments/{id}",
+                             json=request.json,
+                             headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
+    return responce.json()
+
+
+@app.put("/comments/<int:id>")
+@check_login
+def update_comment(id):
+    responce = requests.put(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/comments/{id}",
+                            json=request.json,
+                            headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
+    return responce.json()
+
+
+@app.delete("/comments/<int:id>")
+@check_login
+def delete_comment(id):
+    responce = requests.delete(f"http://{os.getenv('POST_SERVICE_HOST')}:{os.getenv('POST_SERVICE_PORT')}/api/comments/{id}",
+                               json=request.json,
+                               headers={"Content-Type": "application/json", "Token": request.headers.get("Authorization")})
     return responce.json()
